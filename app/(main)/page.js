caa0f04516e82be73ca2ef00f6104c937577f160
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-07-15 02:49:31 +0600

    added dummy btn to home page

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,9 +1,25 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-xl">Landing page coming soon.....!</h1>
+    <main className="flex flex-col items-center justify-center p-24 w-full h-[100vh] gap-5">
+      <h1 className="text-xl font-semibold">Landing page coming soon.....!</h1>
+      <Button
+        color="primary"
+        variant="shadow"
+        type="submit"
+        className=" float-end mt-5"
+        radius="sm"
+        onClick={() => router.push("/trademark-register")}
+      >
+        Register Trademark
+      </Button>
     </main>
   );
 }
