85a3a2caf1831af25d51beb678559add074380d8
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-09-03 01:46:13 +0600

    fixed noted

--- components/form/steps/StepOne.jsx
+++ components/form/steps/StepOne.jsx
@@ -688,9 +688,9 @@ const StepOne = () => {
         <p className="text-md max-md:text-sm text-slate-800 mb-16 mt-7 font-medium">
           <span className="text-red-500 font-bold">Note:</span> Once your search
           results have been reviewed and our specialists have curated your
-          trademark application, Secure Your Mark will collect the necessary
-          fees and pay the discounted TEAS Standard electronic filing fee of
-          $350 on your behalf.
+          trademark application, Legal Trademark Office will collect the
+          necessary fees and pay the discounted TEAS Standard electronic filing
+          fee of $350 on your behalf.
         </p>
       </form>
     </section>
--- components/form/steps/StepThree.jsx
+++ components/form/steps/StepThree.jsx
@@ -77,7 +77,7 @@ const StepThree = () => {
           <p className="text-md max-md:text-sm text-slate-800 mb-16 mt-7 font-medium">
             <span className="text-red-500 font-bold">Note:</span> Once your
             search results have been reviewed and our specialists have curated
-            your trademark application, Secure Your Mark will collect the
+            your trademark application, Legal Trademark Office will collect the
             necessary fees and pay the discounted TEAS Standard electronic
             filing fee of $350 on your behalf.
           </p>
