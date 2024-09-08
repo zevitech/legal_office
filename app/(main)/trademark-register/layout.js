fb17973fd4470dc0ab9ac9cf251f4ead2b5ec631
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-09-08 20:00:46 +0600

    updated tag manager

--- app/(main)/trademark-register/layout.js
+++ app/(main)/trademark-register/layout.js
@@ -32,16 +32,18 @@ export default function RootLayout({ children }) {
             `,
         }}
       />
+
       {/* Google Tag Manager */}
       <Script
         id="gtm"
+        type="text/javascript"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KJGHNHGM');`,
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KJGHNHGM');`,
         }}
       />
 
@@ -61,7 +63,6 @@ export default function RootLayout({ children }) {
             `,
         }}
       />
-
       <body className={`${inter.className}`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
