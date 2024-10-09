16ffdb20d836a1a0ea0d50c75e583f63fb6862fa
Dev Hassan <hassan@xtarlabs.com>
2024-10-09 16:30:49 -0700

    tweak again in different layout file

--- app/(main)/trademark-register/layout.js
+++ app/(main)/trademark-register/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
             `,
         }}
       />
-      
+
       {/* Google Tag Manager */}
       <Script
         id="gtm"
@@ -63,6 +63,16 @@ export default function RootLayout({ children }) {
         }}
       />
 
+      {/* Facebook Pixel */}
+      <Script
+        id="facebook-pixel"
+        type="text/javascript"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1778222035980026');fbq('track', 'PageView');`,
+        }}
+      />
+
       <body className={`${inter.className}`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -73,6 +83,18 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+
+        {/* Non-script (noscript) Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1778222035980026&ev=PageView&noscript=1"
+            alt="facebook-pixel"
+          />
+        </noscript>
+
         <div className={`bg-form-body`}></div>
         <GlobalProvider>{children}</GlobalProvider>
       </body>
