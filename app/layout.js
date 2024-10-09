37f69679dd49528e83fde27e8f29ba44cdcd0c49
Dev Hassan <hassan@xtarlabs.com>
2024-10-09 15:20:03 -0700

    update pixel tag

--- app/layout.js
+++ app/layout.js
@@ -18,78 +18,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Tawk.to */}
-      {/* <Script
-        id="tawkTo"
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/669c6bd9becc2fed69284ba7/1i39gaj7r';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-              })();
-            `,
-        }}
-      /> */}
+      <head>
+        <meta
+          name="keywords"
+          content="where to register business name, trademark my name, trademark brand name, apply for trademark online, trade mark a company name, file my trademark, brand name trademark registration, best trademark registration service, trademark filing company, cheap trademark registration, trade mark registration, get a trademark, register my business, register my company name, trademark my logo, trademark my business name, apply for trademark, file trademark for business name, trademark registration, register my business name, register business name, trademark a name, file a trademark, get my brand trademarked, trademark and brand registration"
+        />
+        <meta name="author" content="Legal Trademark Office" />
 
-      {/* Google Tag Manager */}
-      {/* <Script
-        id="gtm"
-        type="text/javascript"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KJGHNHGM');`,
-        }}
-      /> */}
-
-      {/* MouseFlow Tracking */}
-      {/* <Script
-        id="mfq"
-        type="text/javascript"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: ` window._mfq = window._mfq || [];
-              (function() {
-                var mf = document.createElement("script");
-                mf.type = "text/javascript"; mf.defer = true;
-                mf.src = "//cdn.mouseflow.com/projects/6d88ebfa-5138-492c-b0b1-2f54e50b2048.js";
-                document.getElementsByTagName("head")[0].appendChild(mf);
-              })();
-            `,
-        }}
-      /> */}
-
-      {/* OTHER TAG IMPLEMENTATION */}
-      {/* <Script
-        id="facebook-pixel"
-        strategy="afterInteractive"
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1778222035980026');
-fbq('track', 'PageView');
-            `,
-        }}
-      /> */}
-
-      <Script id="facebook-pixel" strategy="afterInteractive">
-        {`
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
           !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -101,29 +39,9 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 fbq('init', '1778222035980026');
 fbq('track', 'PageView');
         `}
-      </Script>
-
-      <head>
-        <meta
-          name="keywords"
-          content="where to register business name, trademark my name, trademark brand name, apply for trademark online, trade mark a company name, file my trademark, brand name trademark registration, best trademark registration service, trademark filing company, cheap trademark registration, trade mark registration, get a trademark, register my business, register my company name, trademark my logo, trademark my business name, apply for trademark, file trademark for business name, trademark registration, register my business name, register business name, trademark a name, file a trademark, get my brand trademarked, trademark and brand registration"
-        />
-        <meta name="author" content="Legal Trademark Office" />
+        </Script>
       </head>
       <body className={`${poppins.className} bg-color-secondary`}>
-        {/* Google Tag Manager (noscript) */}
-        {/* <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KJGHNHGM"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript> */}
-        {/* End Google Tag Manager (noscript) */}
-
-        <GlobalProvider>{children}</GlobalProvider>
-
         {/* Non-script (noscript) Pixel */}
         <noscript>
           <img
@@ -131,9 +49,10 @@ fbq('track', 'PageView');
             width="1"
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1778222035980026&ev=PageView&noscript=1"
-            alt="facebook-pixel"
           />
         </noscript>
+
+        <GlobalProvider>{children}</GlobalProvider>
       </body>
     </html>
   );
