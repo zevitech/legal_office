7099da340c1c44c4384e2a2d5d49cbb323bbb183
Dev Hassan <hassan@xtarlabs.com>
2024-10-09 16:23:22 -0700

    update script file again

--- app/layout.js
+++ app/layout.js
@@ -18,13 +18,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* Facebook Pixel */}
       <Script
         id="facebook-pixel"
         type="text/javascript"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-           !function(f,b,e,v,n,t,s)
+           (!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -33,7 +34,7 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '1778222035980026');
-fbq('track', 'PageView');
+fbq('track', 'PageView'));
           `,
         }}
       />
