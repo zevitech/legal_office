13104c8debd0757eff485c81a9267ea9cbc6a245
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-10-22 16:34:06 +0600

    merged code form main-stripe-

--- components/pages/PageLoader.jsx
+++ components/pages/PageLoader.jsx
@@ -4,7 +4,7 @@ import { FaSpinner } from "react-icons/fa6";
 const PageLoader = () => {
   return (
     <div className="w-full h-screen fixed top-0 left-0 bottom-0 backdrop-brightness-50 backdrop-blur-[2px] flex-center z-[99999]">
-      <FaSpinner className=" animate-spin font-bold text-5xl text-blue-700" />
+      <FaSpinner className=" animate-spin font-bold text-5xl text-blue-800" />
     </div>
   );
 };
