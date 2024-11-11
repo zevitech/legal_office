2417e20fe13a139bf33a3999bf0e4b952f46f9be
Dev Hassan <hassan@xtarlabs.com>
2024-11-11 08:22:06 -0800

    updated google tag manager tags

--- app/(main)/trademark-register/layout.js
+++ app/(main)/trademark-register/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M76V8V3F');`,
+          })(window,document,'script','dataLayer','GTM-KJGHNHGM');`,
         }}
       />
 
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M76V8V3F"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJGHNHGM"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
