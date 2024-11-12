795c5a02dcebae06da61250325963cdb42a26264
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2024-11-13 00:47:40 +0600

    making master live

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -23,7 +23,7 @@ import { BiSearch } from "react-icons/bi";
 import Searchbar from "@/components/pages/home/search-bar";
 
 export const metadata = {
-  title: "Legal Trademark Office | US Based trademark register website",
+  title: "Legal Trademark Office | US Based trademark register website.",
   description:
     "At Legal Trademark Office, we offer expert trademark registration services to protect your business name, logo, and brand identity. Get started today!",
 };
