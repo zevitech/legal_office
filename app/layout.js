4fbf19128b0a27b5abb039e32f09f187260142f0
Zevitech-Development <zevitechdevelopment@gmail.com>
2025-03-27 00:08:20 +0500

    new version of live chat

--- app/layout.js
+++ app/layout.js
@@ -58,11 +58,41 @@ export default function RootLayout({ children }) {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-       window._lc = window._lc || {};
-    window.__lc.license = 19098393;
-    window.__lc.integration_name = "manual_onboarding";
-    window.__lc.product_name = "livechat";
-    ;(function(n,t,c){function i(n){return e.h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n._lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+      window._lc = window._lc || {};
+      window.__lc = window.__lc || {};
+      window.__lc.license = 19098393;
+      window.__lc.integration_name = "manual_onboarding";
+      window.__lc.product_name = "livechat";
+
+      (function(n, t, c) {
+        function i(n) {
+          return e.h ? e._h.apply(null, n) : e._q.push(n);
+        }
+
+        var e = {
+          _q: [],
+          _h: null,
+          _v: "2.0",
+          on: function() { i(["on", c.call(arguments)]) },
+          once: function() { i(["once", c.call(arguments)]) },
+          off: function() { i(["off", c.call(arguments)]) },
+          get: function() {
+            if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+            return i(["get", c.call(arguments)]);
+          },
+          call: function() { i(["call", c.call(arguments)]) },
+          init: function() {
+            var s = t.createElement("script");
+            s.async = true;
+            s.type = "text/javascript";
+            s.src = "https://cdn.livechatinc.com/tracking.js";
+            t.head.appendChild(s);
+          }
+        };
+
+        if (!n._lc.asyncInit) e.init();
+        n.LiveChatWidget = n.LiveChatWidget || e;
+      })(window, document, [].slice);
     `,
         }}
       />
